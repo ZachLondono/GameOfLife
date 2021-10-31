f4b7c9ca49c5bd6e194cf94bf0ad3d8b06f5dafb
ZachLondono <zachary.londono5@gmail.com>
2021-10-31 16:54:52 -0400

    GUI plays with 4 steps per second

--- gol_gui.py
+++ gol_gui.py
@@ -2,13 +2,15 @@ import PySimpleGUI as sg
 import math
 from game_of_life import GameOfLife
 import time
+import threading
+import ctypes
 
 class GOLGui:
 	def __init__(self, size : int):
 		self.game = GameOfLife(size)
 		self.size = size
-		self.canvasWidth = 640
-		self.canvasHeight = 480
+		self.canvasWidth = 700
+		self.canvasHeight = 700
 		self.layout = [
 				[
 					sg.Graph(
@@ -24,46 +26,67 @@ class GOLGui:
 		self.window = sg.Window("Game of Life", self.layout, background_color= 'black')
 		self.window.Finalize()
 		self.graph = self.window.Element("graph")
+		self.cells = []
 
 	def drawCells(self) -> None:
 		cellWidth = self.canvasWidth / self.size
 		cellHeight = self.canvasHeight / self.size
-		fillColor = 'black'
+		
+		# Clear the graph
+		for cell in self.cells:
+			self.graph.delete_figure(cell)
+		
+		# Reset list of living cells
+		self.cells = []
+
+		# Redraw all living cells
 		for col in range(self.size):
 			x = col * cellWidth
 			for row in range(self.size):
 				y = row * cellHeight
-
 				cellx, celly = self.getCell(col * cellWidth,row * cellHeight)
 				cell = self.game.positions[cellx,celly]
-				if cell.isAlive: fillColor = 'white'
-				else: fillColor = 'black'
-				self.graph.DrawPolygon([(x,y),(x,y+cellHeight),(x+cellWidth,y+cellHeight),(x+cellWidth,y)], line_color="white", line_width= 5, fill_color=fillColor)
+				if cell.isAlive: 
+					poly = self.graph.DrawPolygon([(x,y),(x,y+cellHeight),(x+cellWidth,y+cellHeight),(x+cellWidth,y)], fill_color='white')
+					self.cells.append(poly)
 	
 	def getCell(self, x, y) -> tuple :
 		cellx = math.floor(x / (self.canvasWidth / self.size))
 		celly = gui.size - 1 - math.floor(y / (self.canvasHeight / self.size))
 		return (cellx,celly)
 
+def run(gui):
+	while True:
+		time.sleep(0.25)
+		gui.game.step()
+		global stop_updater
+		if stop_updater: break
+		print("Sending update")
+		gui.window.write_event_value("update", "val")
+		print("update sent")
+	print("Done updater thread")
+
 if __name__ == "__main__":
 
-	gui = GOLGui(20)
+	gui = GOLGui(50)
 	gui.game.setPulsarPattern()
+	#gui.game.randomizeCells(0.2)
+	#gui.game.setBlinkerPattern()
+	#gui.game.setGliderPattern()
 	gui.drawCells()
 
-	while True:
-		event, values = gui.window.Read()
-		if event in (sg.WIN_CLOSED, 'Exit'):
-			break
-		
-		gui.game.step()
-		gui.drawCells()
+	stop_updater = False 
+	updateThread = threading.Thread(target=run, args=(gui,))
+	updateThread.start()
 
-	#	mouse = values['graph']
-	#	if event == 'graph':
-	#		if mouse == (None, None):
-	#			continue
-	#		
-	#		
-	#		
-	#		
+	while True:
+		event, values = gui.window.read()
+		if event is None:
+			break
+		if event == "update":
+			gui.drawCells()
+			gui.window.refresh()
+	
+	stop_updater = True
+	updateThread.join()
+	print("Done main thread")
